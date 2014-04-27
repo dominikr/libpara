bce06153713926daa31fa7d7f9a17d96bf3ea68e
Dominik Russenberger <dominik.russenberger@gmail.con>
2014-04-27 18:06:25 +0200

    foo

--- debian/libpara.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libpara package
-#
-0 4	* * *	root	[ -x /usr/bin/libpara_maintenance ] && /usr/bin/libpara_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libpara):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libpara" command="/usr/bin/libpara"
